962b000046e451797e283142d63d8fea587df6b6
Aric <arichuang@gmail.com>
2014-07-05 16:46:56 -0700

    bootstrap - install mongodb with -y flag

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -9,7 +9,7 @@ apt-get install -y nodejs npm
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
-apt-get install mongodb-org
+apt-get install -y mongodb-org
 
 rm -rf /var/www
 ln -fs /vagrant /var/www
