91abfa0456bb31b62f3bc79b05dca956bf310e18
Aric Huang <arichuang@gmail.com>
2014-07-05 08:17:56 -0700

    vagrant bootstrap - install mongodb

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,5 +5,11 @@ apt-get install -y git-core
 apt-get install -y emacs
 apt-get install -y nodejs npm
 
+# Install MongoDB from official repo
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-get update
+apt-get install mongodb-org
+
 rm -rf /var/www
 ln -fs /vagrant /var/www
