148a263a43024f00d8a902f05a5dd47d7d76ec61
Aric Huang <arichuang@gmail.com>
2014-07-06 20:36:29 -0700

    async - using named closure

--- async.js
+++ async.js
@@ -1,8 +1,11 @@
+var printNumberLater = function(i) {
+    setTimeout(function() {
+	console.log(i);
+    }, 1000);
+};
+
 for (var i = 0; i < 100; i++) {
-    (function (i) {
-	setTimeout(function () {
-	    console.log(i);
-	}, 1000);
-    })(i);
+    printNumberLater(i);
 }
+
 console.log('You might think this gets printed last.')
